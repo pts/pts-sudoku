2b760061f6f194eb19a84e9e46ef7bf6850b6fea
Peter Szabo <pts@fazekas.hu>
2009-09-05 07:05:09 +0100

    improved pts_sudoku.js

--- pts_sudoku.js
+++ pts_sudoku.js
@@ -4,6 +4,15 @@
 // by pts@fazekas.hu at Fri Sep  4 11:01:47 CEST 2009
 //
 // TODO(pts): Micro-optimize.
+//
+// More advanced sudoku solvering methods:
+// http://www.sudoku.com/boards/viewtopic.php?t=6556&start=0
+//
+// A solver on the web: http://zfacts.com/giz/sudoku.js
+// http://zfacts.com/p/sudoku_solver.html
+//
+// The solver on http://www.omegajunior.net/code/sudokusolver.html
+// seems to be using doMiddleIntersection.
 
 // Instantiate variables in the equation (list of variables) eq. Returns
 // null on contradiction, or the new insti.
@@ -77,7 +86,29 @@ function solve(eqs, vars, callback, insts, insti) {
       break;
     } else {
       ++numChoices;
-      print('CH' + i);
+      if (0) { // !! debug
+        print('CH' + i);
+        var nn = 9;
+        var nnnn = nn * nn;
+        var solution = Array(nnnn);
+        var l, m;
+        for (l = 0; l < nnnn; ++l) {
+          solution[l] = '?';
+          for (m = 0; m < nn; ++m) {
+            if (vars[l * nn + m])
+              solution[l] = m + 1;
+          }
+        }
+        for (l = 0; l < nn; ++l) {
+          var s = '';
+          for (h = 0; h < nn; ++h) {
+            s += solution[l * nn + h] + ',';
+          }
+          print(s);
+        }
+        print('.');
+      }
+
       vars[i] = false;
       insts[insti++] = i;
       // TODO(pts): Pass i as an argument.
@@ -197,6 +228,7 @@ function sudoku(problem, callback, doMiddleIntersection) {
     // TODO(pts): Investigate why enabling doMiddleIntersection makes the
     // algorithm slower for nn=9 (even though it decreases the number of
     // choices). Maybe because the so many extra equations?
+    // TODO(pts): Use doMiddleIntersection lazily.
     
     // Intersection of a middle square and a column. (Middle row ii, middle
     // column jj, column j, full row i, value k).
@@ -293,3 +325,79 @@ var problem3 = [0,8,0,0,0,2,7,0,0,
                 0,0,6,5,0,0,0,3,0];
 // 1,8,9,4,3,2,7,5,6,5,2,3,7,6,9,4,1,8,4,6,7,8,5,1,9,2,3,6,9,1,2,7,4,3,8,5,7,4,5,6,8,3,1,9,2,2,3,8,9,1,5,6,7,4,3,5,4,1,2,7,8,6,9,8,1,2,3,9,6,5,4,7,9,7,6,5,4,8,2,3,1
 print('SUMCH' + sudoku(problem3, print, true));
+
+// http://www.forbeginners.info/sudoku-puzzles/extreme-20.htm
+print('--- 4:')
+var problem4 = [6,0,0,0,0,0,0,4,0,
+                0,0,5,0,0,2,0,0,7,
+                7,2,9,0,0,0,0,0,3,
+                0,9,0,0,4,0,0,0,1,
+                0,0,0,0,6,0,0,0,0,
+                4,0,0,0,8,0,0,7,0,
+                3,0,0,0,0,0,1,6,5,
+                2,0,0,4,0,0,8,0,0,
+                0,5,0,0,0,0,0,0,4];
+print('SUMCH' + sudoku(problem4, print, true));
+
+// Hardest sudoku on 2006-07-11:
+// http://www.usatoday.com/news/offbeat/2006-11-06-sudoku_x.htm
+// Seems to be very easy for the software (0 choices with doMiddleIntersection,
+// 7 choices without it).
+print('--- 5:')
+var problem5 = [8,5,0,0,0,2,4,0,0,
+                7,2,0,0,0,0,0,0,9,
+                0,0,4,0,0,0,0,0,0,
+                0,0,0,1,0,7,0,0,2,
+                3,0,5,0,0,0,9,0,0,
+                0,4,0,0,0,0,0,0,0,
+                0,0,0,0,8,0,0,7,0,
+                0,1,7,0,0,0,0,0,0,
+                0,0,0,0,3,6,0,4,0];
+print('SUMCH' + sudoku(problem5, print, true));
+
+// http://www.sudokuoftheday.co.uk/sudoku.php?ACTION=sotd&RES=1024&LEVEL=A
+print('--- 6:')
+var problem6 = [6,4,0,9,0,0,0,1,5,
+                0,8,9,0,1,0,0,0,2,
+                0,0,7,0,0,8,0,0,0,
+                0,6,0,0,4,0,3,0,0,
+                3,0,1,6,0,0,0,0,0,
+                0,0,0,0,0,0,0,2,0,
+                0,0,0,3,0,0,0,0,0,
+                9,0,0,0,0,5,0,3,0,
+                0,0,0,0,0,0,0,4,0];
+print('SUMCH' + sudoku(problem6, print, true));
+
+// http://www.sudoku.com/boards/viewtopic.php?t=5336&postdays=0&postorder=asc&start=43
+// Lots of more, similar puzzles on the site above.
+// This is really hard (117 choices with doMiddleIntersection, 153 choices
+// without it).
+print('--- 7:')
+var problem7 = [5,0,0,0,0,0,0,0,9,
+                0,2,0,1,0,0,0,7,0,
+                0,0,8,0,0,0,3,0,0,
+                0,4,0,6,0,0,0,0,0,
+                0,0,0,0,5,0,0,0,0,
+                0,0,0,2,0,7,0,1,0,
+                0,0,3,0,0,0,8,0,0,
+                0,6,0,0,0,4,0,2,0,
+                9,0,0,0,0,0,0,0,5];
+// 5,3,6,4,7,2,1,8,9,4,2,9,1,8,3,5,7,6,1,7,8,9,6,5,3,4,2,7,4,1,6,9,8,2,5,3,6,8,2,3,5,1,4,9,7,3,9,5,2,4,7,6,1,8,2,5,3,7,1,9,8,6,4,8,6,7,5,3,4,9,2,1,9,1,4,8,2,6,7,3,5
+//print('SUMCH' + sudoku(problem7, print, true));
+
+// http://www.sudoku.com/boards/viewtopic.php?t=6539&start=0
+// Lots of more, similar puzzles on the site above.
+// This is really hard (580 choices with doMiddleIntersection, 669 choices
+// without it).
+print('--- 8:')
+var problem8 = [0,0,0,0,0,0,0,1,2,
+                0,0,0,0,0,0,0,0,3,
+                0,0,2,3,0,0,4,0,0,
+                0,0,1,8,0,0,0,0,5,
+                0,6,0,0,7,0,8,0,0,
+                0,0,0,0,0,9,0,0,0,
+                0,0,8,5,0,0,0,0,0,
+                9,0,0,0,4,0,5,0,0,
+                4,7,0,0,0,6,0,0,0];
+// 8,3,9,4,6,5,7,1,2,1,4,6,7,8,2,9,5,3,7,5,2,3,9,1,4,8,6,3,9,1,8,2,4,6,7,5,5,6,4,1,7,3,8,2,9,2,8,7,6,5,9,3,4,1,6,2,8,5,3,7,1,9,4,9,1,3,2,4,8,5,6,7,4,7,5,9,1,6,2,3,8
+print('SUMCH' + sudoku(problem8, print, false));
