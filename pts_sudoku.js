2dcd3200f5e5f4bda4971e8f30fb14954112698b
Peter Szabo <pts@fazekas.hu>
2012-05-13 09:04:33 +0100

    improved pts_sudoku.js

--- pts_sudoku.js
+++ pts_sudoku.js
@@ -285,6 +285,20 @@ function sudoku(problem, callback, doMiddleIntersection) {
   return solve(eqs, vars, lowCallback, Array(nn * nnnn), 0);
 }
 
+print('--- C:')
+var problemc = [0,0,0,2,7,0,6,0,0,
+                5,0,0,0,0,8,0,0,4,
+                0,6,0,0,0,9,5,0,0,
+                0,0,0,7,0,0,0,0,1,
+                7,8,0,0,9,0,0,4,2,
+                3,0,0,0,0,6,0,0,0,
+                0,0,8,1,0,0,0,5,0,
+                6,0,0,9,0,0,0,0,3,
+                0,0,4,0,2,3,0,0,0];
+print('SUMCH' + sudoku(problemc, print));
+zzzz;
+
+
 print('--- 1:')
 var problem1 = [1,0,0,0,
                 0,0,0,2,
@@ -293,6 +307,7 @@ var problem1 = [1,0,0,0,
 // 1,2,3,4,3,4,1,2,4,1,2,3,2,3,4,1
 print('SUMCH' + sudoku(problem1, print));
 
+if (false) {
 // TODO(pts): Investigate why we need 5 choices here (even with
 // doMiddleIntersection).
 //
@@ -402,3 +417,30 @@ var problem8 = [0,0,0,0,0,0,0,1,2,
                 4,7,0,0,0,6,0,0,0];
 // 8,3,9,4,6,5,7,1,2,1,4,6,7,8,2,9,5,3,7,5,2,3,9,1,4,8,6,3,9,1,8,2,4,6,7,5,5,6,4,1,7,3,8,2,9,2,8,7,6,5,9,3,4,1,6,2,8,5,3,7,1,9,4,9,1,3,2,4,8,5,6,7,4,7,5,9,1,6,2,3,8
 print('SUMCH' + sudoku(problem8, print, false));
+}
+
+// http://ioccc.org/2005/aidan/insane5.sudoku
+print('--- 9:')
+var problem9 = [0,0,0,0,0,2,0,0,0,
+                0,0,1,0,5,6,0,0,0,
+                4,5,6,3,0,0,0,0,0,
+                3,0,0,7,0,9,0,4,5,
+                0,0,0,0,0,0,6,0,0,
+                0,0,0,0,0,0,9,0,0,
+                5,0,0,0,0,8,7,0,2,
+                0,3,0,0,0,0,0,0,4,
+                8,0,2,5,4,0,0,0,0];
+print('SUMCH' + sudoku(problem9, print, true));
+
+// http://ioccc.org/2005/aidan/insane4.sudoku
+print('--- 9:')
+var problema = [0,0,0,2,0,9,0,0,0,
+                0,0,7,0,0,0,0,0,3,
+                3,0,0,0,0,0,0,5,6,
+                0,5,0,0,0,0,0,0,0,
+                0,9,0,0,0,0,3,6,5,
+                0,4,0,0,0,6,7,8,0,
+                0,0,0,6,0,0,0,4,0,
+                0,0,4,0,2,5,8,0,7,
+                9,0,0,0,8,0,0,0,0];
+print('SUMCH' + sudoku(problema, print, true));
